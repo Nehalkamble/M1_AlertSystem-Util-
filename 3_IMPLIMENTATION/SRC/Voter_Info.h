270bd2dcf5081c70986424d0b8294eef23363f52
nehalkamble <kamblenehal12345678@gmail.com>
2022-02-11 00:03:26 +0530

    INIT

--- 3_IMPLIMENTATION/SRC/Voter_Info.h
+++ 3_IMPLIMENTATION/SRC/Voter_Info.h
@@ -1,22 +1,21 @@
 #ifndef voter_info_H
 #define voter_info_H
 
-
-
-typedef struct{
+typedef struct
+{
     char VoterName[50][100];
     int VoterID[50];
     int VoterMNo[50];
-}voterId;
+} voterId;
 
-typedef struct{
+typedef struct
+{
     char CdName[5][20];
     char CdParty[5][20];
     char CdSymbol[5][20];
-}candidateId;
+} candidateId;
 candidateId CdI;
 voterId VdI;
-static int votes[5]={0};
-
+int votes[5];
 
 #endif
