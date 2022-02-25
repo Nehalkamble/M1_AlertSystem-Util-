a7e6bc1fd43e0d69ac1c7bc50b767eb1d9531e63
nehalkamble <kamblenehal12345678@gmail.com>
2022-02-25 15:15:11 +0530

    init

---
+++ 3_IMPLIMENTATION/INC/Voter_Info.h
@@ -0,0 +1,21 @@
+#ifndef voter_info_H
+#define voter_info_H
+
+typedef struct
+{
+    char VoterName[50][100];
+    int VoterID[50];
+    int VoterMNo[50];
+} voterId;
+
+typedef struct
+{
+    char CdName[5][20];
+    char CdParty[5][20];
+    char CdSymbol[5][20];
+} candidateId;
+candidateId CdI;
+voterId VdI;
+int votes[5];
+
+#endif
